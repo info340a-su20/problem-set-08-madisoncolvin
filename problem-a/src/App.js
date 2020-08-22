1a6f5369c8d9b031f7ea459de4628135e94628e8
madisoncolvin <mdcolvin@uw.edu>
2020-08-21 18:48:48 -0700

    solution added

--- problem-a/src/App.js
+++ problem-a/src/App.js
@@ -1,3 +1,4 @@
+/* Madison Colvin */
 import React, { Component } from 'react'; //import React Component
 
 const EXAMPLE_SENATORS = [  
@@ -6,3 +7,70 @@ const EXAMPLE_SENATORS = [
 ];
 
 /* Your code goes here */
+
+
+/* Step one */
+export class App extends Component {
+  render() {
+    let curSenator = this.props.senators;
+    return (
+      <div className = "container">
+        <h1>US Senators 2019</h1>
+        <SenatorTable senators = { curSenator } />
+      </div>
+    );
+  }
+}
+
+/* Step three + step 6 modifications */
+export class SenatorTable extends Component {
+  render() {
+
+    let rows = this.props.senators;
+    let maps = rows.map((senator) => 
+      <SenatorRow key = { senator.name } senator = { senator } />)
+
+    return (
+      <table className = "table table-bordered">
+        <TableHeader cols = { ['Name', 'State', 'Phone', 'Twitter'] } />
+        <tbody>
+          { maps } 
+        </tbody>
+      </table>
+    );
+  }
+}
+
+/* Step four */
+export class TableHeader extends Component {
+  render() {
+
+    let column = this.props.cols;
+    let maps = column.map((colName) => 
+      <th key = { colName }>{ colName }</th>)
+
+    return (
+      <thead>
+        <tr>
+          { maps }
+        </tr>
+      </thead>
+    );
+  }
+}
+
+/* Step five */
+export class SenatorRow extends Component {
+  render() {
+
+    /* Each data feild we are referencing uses: this.props.senator to identify each senator */
+    return (
+      <tr>
+        <td>{this.props.senator.name}</td>
+        <td>{this.props.senator.party.substring(0, 1) + " - " + this.props.senator.state}</td>
+        <td> <a href={ "tel:" + this.props.senator.phone }>{this.props.senator.phone}</a> </td>
+        <td> <a href={ "https://twitter.com/" + this.props.senator.twitter }>{"@" + this.props.senator.twitter}</a> </td>
+      </tr>
+    );
+  }
+}
--- problem-a/src/index.js
+++ problem-a/src/index.js
@@ -1,4 +1,15 @@
+/* Madison Colvin */
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 //render the App component here!
+
+/* Step two */
+import { App } from "./App";
+
+/* Step seven */
+import senators from "./senators";
+
+/* ReactDOM.render() function to render an instance of the App class into the HTML-provided #root element */
+ReactDOM.render(<App senators = { senators } />, document.getElementById("root"));
